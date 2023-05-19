1c170e51e36b91363285445e1dff2c766de3993a
nicolascou <nicolascousillas1@gmail.com>
2023-05-19 12:20:47 +0200

    add get routes

--- index.ts
+++ index.ts
@@ -1,8 +1,28 @@
 import express from "express";
+import axios, { AxiosError } from 'axios';
+
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send('Hello from express and typescript');
+app.get('/', async(req, res) => {
+  const response = await axios.get('https://jsonplaceholder.typicode.com/users');
+  const { data } = response;
+  
+  res.json(data);
+});
+
+app.get('/:userId', async(req, res) => {
+  try {
+    const response = await axios.get(`https://jsonplaceholder.typicode.com/users/${req.params.userId}`);
+    const { data } = response;
+    res.json(data);
+  } catch (error: any) {
+    if (error.response?.status === 404) {
+      res.status(404);
+      res.send('User not found');
+    } else {
+      res.sendStatus(error.response?.status);
+    }
+  }
 });
 
 const port = process.env.PORT || 3000;
